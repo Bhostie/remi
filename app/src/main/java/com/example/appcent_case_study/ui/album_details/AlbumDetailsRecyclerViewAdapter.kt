dbb883c94e3addee21b16ac237a9bf4c4db641ac
Barış Gökmen <barisgokmen06@gmail.com>
2023-05-12 20:24:14 +0300

    Playing music is done

--- app/src/main/java/com/example/appcent_case_study/ui/album_details/AlbumDetailsRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/appcent_case_study/ui/album_details/AlbumDetailsRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.appcent_case_study.ui.album_details
 
+import android.media.MediaPlayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,13 +11,15 @@ import com.example.appcent_case_study.R
 import com.example.appcent_case_study.my_classes.Track
 import com.squareup.picasso.Picasso
 
-class AlbumDetailsRecyclerViewAdapter(private var data: List<Track>, var imgUrl: String) : RecyclerView.Adapter<AlbumDetailsRecyclerViewAdapter.TrackViewHolder>() {
+class AlbumDetailsRecyclerViewAdapter(private var data: List<Track>, private var imgUrl: String) : RecyclerView.Adapter<AlbumDetailsRecyclerViewAdapter.TrackViewHolder>() {
 
+    // Initialize media player class for playing music
+    private val mediaPlayer = MediaPlayer()
 
     class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
 
         var image : ImageView = itemView.findViewById(R.id.cover_image_view)
-        var track_title : TextView = itemView.findViewById(R.id.track_title_text_view)
+        var trackTitle : TextView = itemView.findViewById(R.id.track_title_text_view)
         var duration : TextView = itemView.findViewById(R.id.duration_text_view)
         var icon : ImageView = itemView.findViewById(R.id.icon_image_view)
 
@@ -41,7 +44,8 @@ class AlbumDetailsRecyclerViewAdapter(private var data: List<Track>, var imgUrl:
         Picasso.get().load(imageUrl).into(holder.image)
         holder.icon.setImageResource(R.drawable.ic_baseline_favorite_border_24)
 
-        holder.track_title.text = data[position].title
+
+        holder.trackTitle.text = data[position].title
 
         holder.duration.text = data[position].duration?.let { formatTime(it.toInt()) }
 
@@ -50,6 +54,17 @@ class AlbumDetailsRecyclerViewAdapter(private var data: List<Track>, var imgUrl:
         }
         holder.itemView.setOnClickListener{
             println("YOU CLICKED: ${data[position].title}")
+
+
+            // Reset for previous track, so they will not collide
+            mediaPlayer.reset()
+
+            // Set the data source for the media player with the preview URL
+            mediaPlayer.setDataSource(data[position].preview)
+
+            // Prepare and start the media player
+            mediaPlayer.prepare()
+            mediaPlayer.start()
         }
 
 
@@ -58,7 +73,7 @@ class AlbumDetailsRecyclerViewAdapter(private var data: List<Track>, var imgUrl:
     override fun getItemCount() = data.size
 
 
-    fun formatTime(totalSeconds: Int): String {
+    private fun formatTime(totalSeconds: Int): String {
         val minutes = totalSeconds / 60
         val seconds = totalSeconds % 60
         return String.format("%d:%02d\"", minutes, seconds)
